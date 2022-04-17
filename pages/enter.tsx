458f391b9c2ad5183147c3d509f87f439f2032d6
Juansereina <jsreina@arrivelogistics.com>
2022-04-17 16:44:46 -0500

    sing up

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,47 @@
-export default function EnterPage() {
+import Image from "next/image";
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main>
-      <h1>Sign up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <Image src={"/google.jpg"} alt="Google image" width={30} height={30} />
+      Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
